ae961bda4643ee35ec4c67d233cdd5df6daac922
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2025-10-04 17:20:28 +0200

    Fixed hovering on buttons in the job viewer

--- Assets/Scripts/Jobs/JobDetailsModal.cs
+++ Assets/Scripts/Jobs/JobDetailsModal.cs
@@ -10,12 +10,23 @@ public class JobDetailsModal : MonoBehaviour
     [SerializeField] private TextMeshProUGUI titleText;
     [SerializeField] private TextMeshProUGUI detailsText;
 
+    private CursorManager cursorManager;
+
     private void Awake()
     {
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
 
         if (panel != null) panel.SetActive(false);
+    }
+
+    private void Start()
+    {
+        cursorManager = FindObjectOfType<CursorManager>();
+        if (cursorManager == null)
+        {
+            Debug.LogWarning("CursorManager not found in the scene. Cursor changes will be ignored.");
+        }
     }
 
     public void Show(Job job)
@@ -30,5 +41,6 @@ public class JobDetailsModal : MonoBehaviour
     public void Hide()
     {
         if (panel != null) panel.SetActive(false);
+        cursorManager?.SetHovering(false); // Work around for the fact that the button disappears before OnPointerExit is called
     }
 }
