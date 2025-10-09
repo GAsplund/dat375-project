8dcd3e9362baf810443b9fe27280a0930a37e1dd
Exc <cjx562011@sina.com>
2025-10-09 07:27:28 +0200

    A menu with start button

---
+++ Assets/PlayButton.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public SpriteRenderer ButtonUp;
+    public SpriteRenderer ButtonDown;
+
+    public SceneAsset GameScene;
+   
+    private void OnMouseEnter()
+    {
+        ButtonUp.enabled = false;
+       ButtonDown.enabled = true;
+    }
+
+    private void OnMouseExit()
+    {
+        ButtonUp.enabled = true;
+        ButtonDown.enabled = false;
+    }
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(GameScene.name);
+    }
+    private void Start()
+    {
+        
+    }
+}
