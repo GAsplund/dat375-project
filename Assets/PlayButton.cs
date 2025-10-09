99626d1e28488118727998cd2230ec99af8afadc
JiaxingCao <154356340+JiaxingCao@users.noreply.github.com>
2025-10-09 14:40:42 +0200

    feat: Main menu (#40)
    * A menu with start button
    
    * Update Assets/PlayButton.cs
    
    Co-authored-by: Goostaf <gasplund2@gmail.com>
    
    * Update Assets/PlayButton.cs
    
    Co-authored-by: Goostaf <gasplund2@gmail.com>
    
    * Change mouse behavior a bit
    
    * Change button behavior
    
    * Change to the correct image
    
    ---------
    
    Co-authored-by: Goostaf <gasplund2@gmail.com>

---
+++ Assets/PlayButton.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public SpriteRenderer ButtonUp;
+    public SpriteRenderer ButtonDown;
+
+    public SceneAsset GameScene;
+
+    //private void OnMouseEnter()
+    //{
+    //  ButtonUp.enabled = false;
+    //   ButtonDown.enabled = true;
+    //}
+
+    //private void OnMouseExit()
+    //{
+    //    ButtonUp.enabled = true;
+    //    ButtonDown.enabled = false;
+    //}
+
+
+    private void OnMouseEnter()
+    {
+        ButtonUp.color = new Color(0.8f, 0.8f, 0.8f);
+    }
+
+    private void OnMouseExit()
+    {
+        ButtonUp.color = new Color(1.0f, 1.0f, 1.0f);
+    }
+    private void OnMouseDown()
+    {
+        ButtonUp.enabled = false;
+        ButtonDown.enabled = true;
+    }
+
+    private void OnMouseUp()
+    {
+         SceneManager.LoadScene(GameScene.name);
+    }
+}
