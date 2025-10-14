675b70d341ad994fa59d3c437abd386ecc77ec63
JiaxingCao <154356340+JiaxingCao@users.noreply.github.com>
2025-10-14 15:58:34 +0200

    feat: partly done button (#58)
    * new partly done
    
    * Update Assets/Scripts/Cleaning/LaundryManager.cs
    
    Co-authored-by: Goostaf <gasplund2@gmail.com>
    
    * Update Assets/Scripts/Jobs/JobManager.cs
    
    Co-authored-by: Goostaf <gasplund2@gmail.com>
    
    ---------
    
    Co-authored-by: Goostaf <gasplund2@gmail.com>

--- Assets/Scripts/Cleaning/LaundryManager.cs
+++ Assets/Scripts/Cleaning/LaundryManager.cs
@@ -27,6 +27,8 @@ public class LaundryManager : MonoBehaviour
     // Track progress using the job's items
     private int currentItem = 0;
     private int totalItemsToClean = 0;
+    //currentRewards is used for partly done job
+    private int currentRewards = 0;
 
     void Awake()
     {
@@ -60,6 +62,8 @@ public class LaundryManager : MonoBehaviour
     {
         // Advance to next item from the job
         currentItem++;
+
+        currentRewards += (int)JobManager.GetCurrentJob().reward / totalItemsToClean;
 
         if (currentItem >= totalItemsToClean)
         {
@@ -126,6 +130,26 @@ public class LaundryManager : MonoBehaviour
         yield return new WaitForSeconds(2f);
 
 
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void JobPartlyDone()
+    {//When player click the partly done button
+       StartCoroutine(PartlyDone());    
+    }
+    public IEnumerator PartlyDone()
+    {
+        MoneyManager.Add(currentRewards);
+        Debug.Log($"Job partly completed, rewarded {currentRewards} gold");
+
+        if (approvedText != null)
+        {
+            approvedText.enabled = true;
+            approvedText.text = $"Job partly Done!  rewarded {currentRewards} gold ";
+        }
+
+        JobManager.CompleteCurrentJob();
+        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene(sceneToLoad);
     }
 
