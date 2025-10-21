f145b4aed57fe44b245e18475dd767bfd944b0af
William Wedholm <willewe23@gmail.com>
2025-10-21 09:49:55 +0200

    Feat: Business upgrade (#62)
    * Assets
    
    * Working on Upgrading Business
    
    * Fixed Upgrading
    
    * Update InteractionScene.unity
    
    * Cracked Floor
    
    * Update InteractionScene.unity
    
    * Adjust level positioning and sizing
    
    * Align text better
    
    ---------
    
    Co-authored-by: Goostaf <gasplund2@gmail.com>

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -2,13 +2,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 
-public class InteractableObject : MonoBehaviour
+public class SceneInteractable : MonoBehaviour
 {
-    public string sceneToLoad = "WashScene";
     public KeyCode interactKey = KeyCode.E;
     public TextMeshProUGUI interactText;
+    public int cost = 0;
+    public string sceneToLoad;
 
     private bool playerInRange = false;
+    private int currentMoney = 0;
+
+    void OnEnable() => MoneyManager.OnValueChange += UpdateMoney;
+    void OnDisable() => MoneyManager.OnValueChange -= UpdateMoney;
+
+    void UpdateMoney(int money) => currentMoney = money;
 
     void Start()
     {
@@ -20,7 +27,18 @@ public class InteractableObject : MonoBehaviour
     {
         if (playerInRange && Input.GetKeyDown(interactKey))
         {
-            SceneManager.LoadScene(sceneToLoad);
+            if (currentMoney >= cost)
+            {
+                if (cost > 0)
+                    MoneyManager.Subtract(cost);
+
+                if (!string.IsNullOrEmpty(sceneToLoad))
+                    SceneManager.LoadScene(sceneToLoad);
+            }
+            else
+            {
+                Debug.Log("Not enough money!");
+            }
         }
     }
 
@@ -44,4 +62,3 @@ public class InteractableObject : MonoBehaviour
         }
     }
 }
-
